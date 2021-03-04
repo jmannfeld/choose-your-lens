f878dd5975af3ce62657d171b803b88448725b60
Jordan Mannfeld <jordan.mannfeld@iga.in.gov>
2021-03-03 22:25:50 -0500

    fixes build

--- web/gatsby-node.ts
+++ web/gatsby-node.ts
@@ -28,16 +28,6 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
 
     return graphql<any>(`
         query GatsbyNodeQuery {
-            allMarkdown: allMdx {
-                edges {
-                node {
-                    fields {
-                    slug
-                    sourceName
-                    }
-                }
-                }
-            }
             blog: allSanityPost {
                 edges {
                 node {
@@ -48,13 +38,6 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
                 }
                 }
             }
-            portfolio: allMdx(filter: {fields: {sourceName: {eq: "portfolio"}}}) {
-                edges {
-                node {
-                    id
-                }
-                }
-            }
             limitPost: site {
                 siteMetadata {
                 blogItemsPerPage
@@ -62,19 +45,5 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
                 }
             }
             }
-    `).then(result => {
-        console.log('result.data.allMarkdown.edges', result.data.allMarkdown.edges)
-        result.data.allMarkdown.edges.concat(result.data.blog.edges).forEach(({ node }) => {
-            let template = node.fields !== null && node.fields !== undefined && node.fields.slug !== null && node.fields.slug !== undefined ? node.fields.sourceName : 'blog'
-            console.log('template:', template)
-            createPage({
-                path: node.fields !== null && node.fields !== undefined && node.fields.slug !== null && node.fields.slug !== undefined ? node.fields.slug : `/blog/${node.slug.current}`,
-                component: path.resolve("./src/templates/" + template + ".tsx"),
-                context: {
-                    slug: node.fields !== null && node.fields !== undefined && node.fields.slug !== null && node.fields.slug !== undefined ? node.fields.slug : node.slug.current,
-                },
-            })
-        })
-
-    })
+    `)
 }
