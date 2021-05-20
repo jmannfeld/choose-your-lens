b80859a6803b8a8c0e9167dcf49581f5bf5fc197
Jordan Mannfeld <jordan.mannfeld@iga.in.gov>
2021-05-20 12:35:08 -0400

    adds netlify dashboard and deploy hook to sanity studio

---
+++ studio/plugins/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Choose Your Lens",
+            apiId: "18a24f68-5d54-448e-9e57-88a79d1a2cfd",
+            buildHookId: "60a689ec095e9c7018924ff6",
+            name: "chooseyourlens",
+          },
+        ],
+      },
+    },
+  ],
+};
